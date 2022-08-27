c4ffc8908ba016d3412d4b9c87db2047c760bb92
gxf <18631279680@163.com>
2022-08-27 19:44:22 +0800

    测试提交

--- src/main/java/com/peait/spider/SpiderApplication.java
+++ src/main/java/com/peait/spider/SpiderApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpiderApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(SpiderApplication.class, args);
     }
